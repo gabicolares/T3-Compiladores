3149d35948742b43b971beb3a45533065cc8f03c
gabicolares <105060450+gabicolares@users.noreply.github.com>
2026-06-22 16:49:29 -0300

    Adiciona ParserVal para BYACCJ

---
+++ ParserVal.java
@@ -0,0 +1,29 @@
+//#############################################
+//## file: ParserVal.java
+//## Semantic value used by BYACC/J
+//#############################################
+public class ParserVal {
+    public int ival;
+    public double dval;
+    public String sval;
+    public Object obj;
+
+    public ParserVal() {
+    }
+
+    public ParserVal(int val) {
+        ival = val;
+    }
+
+    public ParserVal(double val) {
+        dval = val;
+    }
+
+    public ParserVal(String val) {
+        sval = val;
+    }
+
+    public ParserVal(Object val) {
+        obj = val;
+    }
+}
